0ad17396aec91b21b905b72cbfa10fb4a46206b3
GitHub Action <action@github.com>
2023-11-01 02:44:40 +0000

    Template cleanup

--- src/main/kotlin/com/github/lexdev7/androidtemplate/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/lexdev7/androidtemplate/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.lexdev7.androidtemplate.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
